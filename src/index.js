1835002f8b18006608fe260de06c35fcc20abc55
ftolentino <filmer.tolentino@gmail.com>
2022-06-30 22:51:35 -0700

    Update styling, randomize color on h1 header

--- src/index.js
+++ src/index.js
@@ -14,13 +14,14 @@ $(document).ready(function () {
     promise.then(function (response) {
       let testString = "";
       const body = JSON.parse(response);
+      console.log(response);
       let index = getRandomInt(249);
 
       testString = body.data[index].images.large;
       let cardPrice = body.data[index].tcgplayer.url;
 
       $("#output").html(
-        `<img class="img-fluid my-4" src="${testString}" /><br><a href="${cardPrice}" target="_blank">Buy card</a>`
+        `<img class="img-fluid my-4" src="${testString}" /><br><a class="display-6 text-decoration-none" href="${cardPrice}" target="_blank">Buy card</a>`
       );
     }),
     function (error) {
@@ -46,6 +47,8 @@ function getElements(response) {
     const body = response;
     let colorVar = "#" + body.colors[index].hex;
     $("#cardColor").css("background-color", colorVar);
+    $(".randomColor").css("color", colorVar);
+
   } else {
     $(".showError").text(
       `There was an error processing your request; ${response}`
